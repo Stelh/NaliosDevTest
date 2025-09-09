0b22089bfddc4c2d5ec825e43bbb1dbc796403c1
Stelh <chrisztw@gmail.com>
2025-09-09 22:16:02 +0200

    recursive return

--- 03_my_sum.py
+++ 03_my_sum.py
@@ -4,7 +4,15 @@ def my_sum(list):
         sum += i
     print(sum)
 
-#recursive soon
-my_sum(
+def my_sum_recursive(list):
+    return list[0] + my_sum_recursive(list[1:]) if list else 0
+
+def my_sum_recursive2(list):
+    if len(list) == 0:
+        return 0
+    return list[0] + my_sum_recursive2(list[1:])
+
+# BONUS: Recursive
+print(my_sum_recursive(
     [1,2,3,4.5]
-)
+))
